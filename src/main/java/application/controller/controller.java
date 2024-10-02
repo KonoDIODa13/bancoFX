6a9b173a766386277e79a3561db99342121c4852
KonoDIODa13 <jaimegonbra1789@gmail.com>
2024-10-02 20:08:26 +0200

    anclando el controller con el fxml

--- src/main/java/application/controller/controller.java
+++ src/main/java/application/controller/controller.java
@@ -1,4 +1,37 @@
 package application.controller;
 
+import application.CRUD.Banco;
+import application.domain.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class controller {
+    @FXML
+    private ToggleGroup tipoCuenta;
+    @FXML
+    private RadioButton RBAhorro;
+    @FXML
+    private RadioButton RBPersonal;
+    @FXML
+    private RadioButton RBEmpresa;
+    @FXML
+    TextField TFNombre, TFApellidos, TFDNI, TFIBAN, TFSaldo, TFInteres, TFMantenimiento, TFInteresDescubierto, TFMaxDescubierto;
+
+    public void mostrarCuentaPersonal(ActionEvent event){
+           }
+
+
+    public void mostrarCuentaEmpresa(ActionEvent event) {
+    }
+
+    public void mostrarCuentaAhorro(ActionEvent event) {
+        TFInteres.setDisable(false);
+        TFMantenimiento.setDisable(true);
+        TFInteresDescubierto.setDisable(true);
+        TFMaxDescubierto.setDisable(true);
+    }
 }
